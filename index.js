4977aec587ab7463fef8a1500d1c03a9239e2617
unknown <zsajid014@gmail.com>
2021-05-18 19:26:16 -0700

    Connected to mongo container using mongoose

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
+
+mongoose
+.connect("mongodb://<username>:<password>@mongo:27017/?authSource=admin")
+.then(() => console.log("Database connected successfully"))
+.catch((e) => console.log("Error"))
 
 app.get("/", (req, res) => {
     res.send("<h2>Hello World!!!</h2>")
